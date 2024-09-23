730c4f02e0d5094a6887ef6894c8d5eaadc5ca03
Anuj <panchalanuj411@gmail.com>
2024-09-24 00:44:56 +0530

    Update deployment file

--- book_rental/deploy.sh
+++ book_rental/deploy.sh
@@ -1,3 +1,16 @@
+sudo apt update
+sudo apt-get install python3-pip git-core virtualenv redis-server telnet nginx
+
+sudo systemctl start redis-server
+sudo systemctl enable redis-server
+
+cd /documents/projects/
+git clone https://github.com/Anuj411/book-rental-system.git
+sudo virtualenv venv
+source ./venv/bin/activate
+
+cd book-rental-system/book_rental
+
 pip install -r ../requirements/production.txt
 
 mv deployment/nginx.conf /etc/nginx/sites-available/project
