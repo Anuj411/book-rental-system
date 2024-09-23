8915ad493c8edab671ca4d8f33972e0b28f1144d
Anuj <panchalanuj411@gmail.com>
2024-09-24 01:04:09 +0530

    updated deploy.sh

--- book_rental/deploy.sh
+++ book_rental/deploy.sh
@@ -1,16 +1,16 @@
 sudo apt update
 sudo apt-get install python3-pip git-core virtualenv redis-server telnet nginx
 
-sudo systemctl start redis-server
-sudo systemctl enable redis-server
-
 pip install -r ../requirements/production.txt
 
-mv deployment/nginx.conf /etc/nginx/sites-available/project
-mv deployment/gunicorn.conf /etc/systemd/system/gunicorn.service
+sudo mv deployment/nginx.conf /etc/nginx/sites-available/project
+sudo mv deployment/gunicorn.conf /etc/systemd/system/gunicorn.service
 
 python manage.py collectstatic
 python manage.py seed
+
+sudo systemctl start redis-server
+sudo systemctl enable redis-server
 
 sudo systemctl daemon-reload
 
