032d56c4baadb7cabfe335a4fcb6f1e537d6980d
Anuj <panchalanuj411@gmail.com>
2024-09-29 12:21:05 +0530

    Add docker file

--- book_rental/deploy.sh
+++ book_rental/deploy.sh
@@ -1,10 +1,13 @@
 sudo apt update
-sudo apt-get install python3-pip python3-dev git-core virtualenv redis-server default-libmysqlclient-dev  build-essential  pkg-config telnet nginx
+sudo apt-get install python3-pip python3-dev git-core virtualenv redis-server default-libmysqlclient-dev build-essential pkg-config telnet nginx
 
 pip install -r ../requirements/production.txt
 
-sudo mv ./deployment/nginx.conf /etc/nginx/sites-available/project
-sudo mv ./deployment/gunicorn.conf /etc/systemd/system/gunicorn.service
+sudo cp ./deployment/nginx.conf /etc/nginx/sites-available/project
+sudo cp ./deployment/gunicorn.conf /etc/systemd/system/gunicorn.service
+
+sudo ln -s /etc/nginx/sites-available/project /etc/nginx/sites-enabled
+sudo nginx -t
 
 python manage.py collectstatic
 python manage.py seed
@@ -17,5 +20,3 @@ sudo systemctl daemon-reload
 sudo systemctl restart nginx
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
-
-sudo systemctl daemon-reload
